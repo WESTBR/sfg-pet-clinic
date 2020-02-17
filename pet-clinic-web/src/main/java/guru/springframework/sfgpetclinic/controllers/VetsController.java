48fbc91f18a0ddec28fff8468d1f50ac3f75182b
WESTBR <danyngongang@gmail.com>
2020-02-17 14:56:18 +0100

    modifying VetController to get list of all vets . cLoses #15.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
@@ -1,15 +1,24 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/vet","/vets"})
 @Controller
 public class VetsController {
 
-    @RequestMapping({"","/vet","/index","/index.html"})
-    public String listVets(){
+    private final VetService vetService;
 
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"","/vet","/index","/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
 
     }
