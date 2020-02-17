876a1dc151a60b2b5c73d97b4a48787e601afc54
WESTBR <danyngongang@gmail.com>
2020-02-17 22:29:02 +0100

    Links bugs fixed . cLoseS #45.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping({"/vet","/vets"})
+@RequestMapping({"/vet","/vets","/vets.html"})
 @Controller
 public class VetsController {
 
